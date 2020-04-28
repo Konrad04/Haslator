1d91e74ac26349f6557a10a58ed7235b59ca6b24
Konrad04 <konradrygosik@gmail.com>
2020-04-28 10:30:46 +0200

    Ulepszona wersja

--- Haslator.js
+++ Haslator.js
@@ -1,8 +1,20 @@
-
 function weryfikacja() {
     var passy = document.forms['haslator']['haslo'];
-    var idealne = /(?=.*\d).{7,}/;
-    var mierne = /(?=.*\d).{4,}/;
+
+    var idealne = /(?=.*\W)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    var dobreA = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    var dobreB = /(?=.*\d)(?=.*[a-z])(?=.*\W).{6,}/;
+    var dobreC = /(?=.*\d)(?=.*[A-Z])(?=.*\W).{6,}/;
+    var dobreD = /(?=.*\W)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+
+    var mierneA = /(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    var mierneB = /(?=.*[a-z])(?=.*\d).{6,}/;
+    var mierneC = /(?=.*[a-z])(?=.*\W).{6,}/;
+    var mierneD = /(?=.*[A-Z])(?=.*\d).{6,}/;
+    var mierneE = /(?=.*[A-Z])(?=.*\W).{6,}/;
+    var mierneF = /(?=.*\d)(?=.*\W).{6,}/;
+
 
 
     if (passy.value.length < 1) {
@@ -12,14 +24,15 @@ function weryfikacja() {
     }
 
 
-    else if (passy.value.match(idealne)) {
+    else if ((passy.value.match(idealne)) || (passy.value.match(dobreA)) || (passy.value.match(dobreB)) || (passy.value.match(dobreC)) || (passy.value.match(dobreD))) {
         document.getElementById("komunikat").innerHTML = "Dobre hasło";
         komunikat.style.color = "green";
         return false;
     }
 
 
-    else if (passy.value.match(mierne)) {
+    else if ((passy.value.match(mierneA)) || (passy.value.match(mierneB)) || (passy.value.match(mierneC)) ||
+        (passy.value.match(mierneD)) || (passy.value.match(mierneE)) || (passy.value.match(mierneF))) {
         document.getElementById("komunikat").innerHTML = "Średnie hasło";
         komunikat.style.color = "blue";
         return false;
@@ -29,6 +42,7 @@ function weryfikacja() {
         document.getElementById("komunikat").innerHTML = "Słabe hasło";
         komunikat.style.color = "yellow";
         return false;
+
     }
 }
 
